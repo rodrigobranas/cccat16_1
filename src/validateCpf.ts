d164c16320d698ead6a9ce51904de6d619ccc8fd
Rodrigo Branas <rodrigo.branas@gmail.com>
2024-04-01 17:48:22 -0300

    updated

--- src/validateCpf.ts
+++ src/validateCpf.ts
@@ -1,20 +1,60 @@
-function extractDigit (cpf: string, factor: number) {
-	let total = 0;
-	for (const digit of cpf) {
-		if (factor > 1) total += parseInt(digit) * factor--;
-	}
-	const rest = total%11;
-	return (rest < 2) ? 0 : 11 - rest;
-}
+export function validateCpf (str: string) {
+	if (str !== null) {
+		if (str !== undefined) {
+			if (str.length >= 11 && str.length <= 14){
+				// cleaning cpf
+				str=str
+					.replace('.','')
+					.replace('.','')
+					.replace('-','')
+					.replace(" ","");  
+				// tudo igual
+				if (!str.split("").every(c => c === str[0])) {
+					try{  
+						let     d1, d2;  
+						let     dg1, dg2, rest;  
+						let     digito;  
+							let     nDigResult;  
+						d1 = d2 = 0;  
+						dg1 = dg2 = rest = 0;  
+							
+						for (let nCount = 1; nCount < str.length -1; nCount++) {  
+							// if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
+							// 	return false;
+							// } else {
+	
+								digito = parseInt(str.substring(nCount -1, nCount));  							
+								d1 = d1 + ( 11 - nCount ) * digito;  
+				
+								d2 = d2 + ( 12 - nCount ) * digito;  
+							// }
+						};  
+							
+						rest = (d1 % 11);  
+						
+						// se for menor que 2 é 0, senão é 11 menos o resto
+						dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;  
+						d2 += 2 * dg1;  
+						rest = (d2 % 11);  
+						if (rest < 2)  
+							dg2 = 0;  
+						else  
+							dg2 = 11 - rest;  
+				
+							let nDigVerific = str.substring(str.length-2, str.length);  
+						nDigResult = "" + dg1 + "" + dg2;  
 
-export function validate (cpf: string) {
-	if (!cpf) return false;
-	cpf=cpf.replace('.','').replace('.','').replace('-','').replace(" ","");  
-	if (cpf.length !== 11) return false;
-	if (cpf.split("").every(c => c === cpf[0])) return false; 
-	const dg1 = extractDigit(cpf, 10);
-	const dg2 = extractDigit(cpf, 11);
-	let nDigVerific = cpf.slice(9);
-	const nDigResult = `${dg1}${dg2}`;  
-	return nDigVerific == nDigResult;
+						return nDigVerific == nDigResult;
+						
+					// se der problema...
+					}catch (e){  
+						console.error("Erro !"+e);  
+	
+						return false;  
+					}  
+				} else return false;
+	
+			}else return false;
+		} else return false;
+	} else return false;
 }
